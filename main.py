176bbbdd243d788258e7967423e87e8e2273a3c5
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2021-12-22 20:42:23 +0500

    Start on 0.0.0.0 by default

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import grpc
 import os
 import currency_service_pb2_grpc
+import logging
 from concurrent import futures
 from service import DummyProvider
 
 if __name__ == '__main__':
-    host = os.getenv('HOST', '[::1]')
+    host = os.getenv('HOST', '0.0.0.0')
     port = os.getenv('PORT', '50000')
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=6))
@@ -13,5 +14,6 @@ if __name__ == '__main__':
         DummyProvider(), server)
 
     server.add_insecure_port(f'{host}:{port}')
+    logging.info('Starting GRPC server')
     server.start()
     server.wait_for_termination()
